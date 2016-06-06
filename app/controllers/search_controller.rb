02c46e1276a83d50a33d348e1fc2155da1150a8f
Alireza Andersen <alireza.andersen@gmail.com>
2016-06-06 10:25:26 -0600

    still trying to figure out pagination

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,19 @@
 class SearchController < ApplicationController
 
   def index
-    @stations = Station.find_by(params)
+    stations = Station.find_by(params)
+    binding.pry
+     @stations = WillPaginate::Collection.create(params[:page], params[:per_page], stations.length) do |pager|
+      pager.replace stations[pager.offset, pager.per_page]
+    end
   end
 
+  # def index
+  #    @users = User.paginate(:page => params[:page], :per_page => 5)
+  #  end
+
+#    products = %w(i love ruby on rails)
+# @products = WillPaginate::Collection.create(params[:page], params[:per_page], products.length) do |pager|
+#    pager.replace products[pager.offset, pager.per_page]
+# end
 end
